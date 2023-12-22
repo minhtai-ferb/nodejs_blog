62e1260f0153958dcfce75c67e1920b28048a1e4
Tai Minh <minhtai7112003@gmail.com>
2023-12-22 17:51:19 +0700

    sass

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const handlebars = require('express-handlebars');
+const handlebars = require('express-handlebars')
 
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.use(morgan('combined'));
 
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+
+app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
   res.render('home');
