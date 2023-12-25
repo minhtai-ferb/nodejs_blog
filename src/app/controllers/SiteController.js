b4f7c2f29fc635c0e06c0411c263e0d77b95cb1a
Tai Minh <minhtai7112003@gmail.com>
2023-12-25 17:28:46 +0700

    Refactor Code: create Contructor

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,13 @@
+class SiteController {
+  //[GET] /
+  index(req, res) {
+    res.render("home");
+  }
+
+  //[GET] /news/:slug
+  // show(req, res) {
+  //   res.send("NEW DETAILS");
+  // }
+}
+
+module.exports = new SiteController();
--- src/index.js
+++ src/index.js
@@ -1,26 +1,37 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const handlebars = require('express-handlebars')
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const route = require("./routes");
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
+// Routes init
+route(app);
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs'
-}));
-app.set('view engine', 'hbs');
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-app.set('views', path.join(__dirname, 'resources/views'));
+app.use(express.json());
+
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
+
+app.set("views", path.join(__dirname, "resources/views"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+});
